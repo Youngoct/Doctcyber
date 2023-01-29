08cc919833fbeee3f2307e57d0b93121b1bf15d2
Youngoct <youngcoder42@gmail>
2023-01-29 04:17:01 +0100

    main.c

---
+++ C_programming/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - print A to Z
+*Return: 0 as success
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 'A'; i <= 'Z'; i++)
+	{
+		putchar(i);
+		putchar('\n');
+	}
+	ruturn: 0;
+}
+
