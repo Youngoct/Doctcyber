86bd27605ac970c9812eac7bd5b69abe0937ace6
Youngoct <youngcoder42@gmail>
2023-02-13 23:02:08 +0100

    code.c

---
+++ C_programming/format.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print the format
+ *
+ *Return: 0 as success
+ */
+
+int main(void)
+{
+	char s = 'G';
+	int z = 340 + 60;
+	float l = 40.0 + 0.40;
+
+	printf("value of s = %C\n", s);
+	printf("value of z = %i\n", z);
+	printf("value of l = %f\n", l);
+	return (0);
+}
---
+++ C_programming/if_else.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+*main - print the result
+*
+*Return:0 as success
+*/
+int main(void)
+{
+
+	int w = 1100;
+	int z = 1550;
+	
+	if(z > s);
+	{
+	printf("yes\n");
+	}
+
+	else
+	{
+	printf("no\n");
+	}
+	return(0);
+{
+	
